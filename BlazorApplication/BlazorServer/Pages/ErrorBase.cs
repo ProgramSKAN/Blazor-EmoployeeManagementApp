b5105de43a3e93b3dfdcd6aa15d430acffbf794a
ProgramSKAN <59524137+ProgramSKAN@users.noreply.github.com>
2020-11-13 14:08:13 +0530

    Add files via upload

---
+++ BlazorApplication/BlazorServer/Pages/ErrorBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Pages
+{
+    public class ErrorBase : ComponentBase
+    {
+        protected string mode = "Development Mode";
+    }
+}
